cb3e1e912f6caae3476349475a3c55d93547bd9d
Vlad-Eris <vlad.mihai@emanuel.ro>
2024-01-20 13:54:23 +0200

    TabelSI

---
+++ TabelSi/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TabelSi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("| A | B | A & B |");
+            Console.WriteLine("-----------------");
+
+            for (int A = 0; A <= 1; A++)
+            {
+                for (int B = 0; B <= 1; B++)
+                {
+                    int rezultat = A & B;
+                    Console.WriteLine($"| {A}| {B} |{rezultat}| ");
+
+                    Console.WriteLine("---------------");
+
+
+                }
+
+            }
+
+        }
+    }
+}
